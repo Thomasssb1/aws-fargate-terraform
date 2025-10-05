c8eae1cb63b4af9c90941f224b545ed17b4ccaf6
Thomas Beer <thomas.beer04@outlook.com>
2025-10-05 18:20:07 +0100

    Create cloudwatch & WAF in main

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,10 @@ module "vpc" {
   source            = "./modules/vpc"
   availability_zone = var.region // Automatically adds a & b for public and private
 }
+
+module "cloudwatch" {
+  source                          = "./modules/cloudwatch"
+  waf_log_group_name              = "waf-log-group-${var.region}"
+  waf_log_group_retention_in_days = 30
+  tags                            = {}
+}
