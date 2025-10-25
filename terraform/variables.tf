7c2a0400150475a7a8edf0dd836aa264fb4755eb
Thomas Beer <thomas.beer04@outlook.com>
2025-10-25 20:05:50 +0100

    Add variables for ECS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,39 @@ variable "container_name" {
   type        = string
   default     = "main-container"
 }
+
+variable "container_image" {
+  description = "Docker image to deploy"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "task_count" {
+  description = "Number of ECS tasks to run"
+  type        = number
+  default     = 2
+}
+
+
+variable "container_port" {
+  description = "Port the container listens on"
+  type        = number
+  default     = 5000
+}
+
+variable "cpu" {
+  description = "CPU units for the ECS task"
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "Memory (in MiB) for the ECS task"
+  type        = number
+  default     = 512
+}
